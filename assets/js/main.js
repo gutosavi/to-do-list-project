08823d1f3a146b67462c3f35c301d450a746aff7
Gustavo Savi <gutosavi@hotmail.com>
2026-01-06 14:42:38 -0300

    create a scripts

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+const inputTexto = document.querySelector('#inTexto');
+const btn = document.querySelector('#btn');
+const tarefas = document.querySelector('#tarefas');
+
+function btnEnter(e){
+    if (e.key === 'Enter' || e.type === 'click'){
+        if (inputTexto.value === ''){
+            alert('Você deve digitar algo para adicionar')
+            return
+        } else {
+            criaTarefa(inputTexto.value)
+        }
+    }
+};
+
+btn.addEventListener('click', btnEnter);
+inputTexto.addEventListener('keypress', btnEnter);
+
+function criaTarefa(texto){
+    const li = criaLi();
+    li.innerText = texto;
+    tarefas.appendChild(li);
+    limpar();
+};
+
+function criaLi(){
+    const li = document.createElement('li');
+    return li
+};
+
+function limpar(){
+    inputTexto.value = '';
+    inputTexto.focus();
+};
